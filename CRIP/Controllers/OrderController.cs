8e15670a4dfcad63c73c8e20828a78e3a45088af
yusong liu <944860844@qq.com>
2023-04-05 21:14:50 +0800

    test

--- CRIP/Controllers/OrderController.cs
+++ CRIP/Controllers/OrderController.cs
@@ -47,20 +47,57 @@ namespace CRIP.Models
         {
             return Ok(await _CartRepository.GetOrdersByOrderIdAsync(orderId));
         }
-       // [HttpPost("payment/{orderId}")]
-      //  [Authorize(AuthenticationSchemes = "Bearer")]
-      //  public async Task<IActionResult> paymentOrder(
-      //      [FromRoute] string orderId)
-      //  {
-     //       PagesResponse pagesResponse = new();
-     //       var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-     //       if (orderId == null)
-     //       {
-     //           pagesResponse.BadRequest("出错，检查订单id");
-     //           return Ok(pagesResponse);
-     //       }
-    //        var order =await _CartRepository.GetOrdersByOrderIdAsync(orderId);
-    //        order.
-    //    }
+
+        [HttpPost("payment/{orderId}")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> paymentOrder(
+            [FromRoute] string orderId)
+        {
+            PagesResponse pagesResponse = new();
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (orderId == null)
+            {
+                pagesResponse.BadRequest("出错，检查订单id");
+                return Ok(pagesResponse);
+            }
+            var order = await _CartRepository.GetOrdersByOrderIdAsync(orderId);
+
+            if (order == null)
+            {
+                pagesResponse.NotFound("订单不存在");
+                return Ok(pagesResponse);
+            }
+
+            // 根据订单状态调用相应的方法
+            switch (order.State)
+            {
+                case OrderStateEnum.Pending:
+                    order.PaymentProcessing();
+                    //await _CartRepository.SaveChangesAsync();
+                    pagesResponse.Success("订单已支付，请等待处理结果");
+                    break;
+                case OrderStateEnum.Processing:
+                    pagesResponse.Success("订单正在处理中，请等待处理结果");
+                    break;
+                case OrderStateEnum.Completed:
+                    pagesResponse.Success("订单已支付成功");
+                    break;
+                case OrderStateEnum.Declined:
+                    pagesResponse.BadRequest("订单支付失败，请重新支付");
+                    break;
+                case OrderStateEnum.Cancelled:
+                    pagesResponse.BadRequest("订单已取消");
+                    break;
+                case OrderStateEnum.Refund:
+                    pagesResponse.Success("订单已退款");
+                    break;
+                default:
+                    pagesResponse.BadRequest("订单状态错误");
+                    break;
+            }
+
+            return Ok(pagesResponse);
+
+        }
     }
 }
