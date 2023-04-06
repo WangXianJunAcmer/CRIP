2812ea738d0b5e229fdcd35387ea546cbebcbbfe
Jiasmiao <1718949950@qq.com>
2023-04-06 19:01:03 +0800

    寻找页面的跳转与返回

--- vite-project/src/router/index.ts
+++ vite-project/src/router/index.ts
@@ -1,98 +1,85 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import { useTestStore } from "../store";
 
-
 const routes: Array<RouteRecordRaw> = [
+  {
+    //地址和component是必传的
+    path: "/Main",
+    name: "Main",
+    component: () => import("../views/Main/index.vue"),
+  },
+  {
+    //地址和component是必传的
+    path: "/test",
+    name: "test",
+    component: () => import("../views/test.vue"),
+  },
 
-    {
-        //地址和component是必传的
-        path: '/Main',
-        name: 'Main',
-        component: () => import('../views/Main/index.vue'),
-    },
-    {
-        //地址和component是必传的
-        path: '/test',
-        name: 'test',
-        component: () => import('../views/test.vue'),
-    },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/login/Login.vue"),
+  },
+  {
+    path: "/Reg",
+    name: "Reg",
+    component: () => import("@/views/login/register.vue"),
+  },
 
+  {
+    path: "/shop",
+    name: "shop",
 
-    {
-        path: '/login',
-        name: 'Login',
-        component: () => import('@/views/login/Login.vue'),
+    component: () => import("@/views/shop/index.vue"),
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    meta: { requiresAuth: true },
+    component: () => import("@/views/shopcart/index.vue"),
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: () => import("@/views/search/search.vue"),
+  },
+  {
+    path: "/consult",
+    name: "consult",
+    component: () => import("@/views/consult/index.vue"),
+  },
+  {
+    path: "/doctor",
+    name: "doctor",
+    component: () => import("@/views/consult/doctor.vue"),
+  },
+];
 
-    },
-    {
-        path: '/Reg',
-        name: 'Reg',
-        component: () => import('@/views/login/register.vue'),
-
-    },
-
-    {
-        path: '/shop',
-        name: 'shop',
-        
-        component: () => import('@/views/shop/index.vue'),
-
-    },
-    {
-        path: '/cart',
-        name: 'cart',
-        meta:{requiresAuth:true},
-        component: () => import('@/views/shopcart/index.vue'),
-
-    },
-    {
-        path: "/consult",
-        name: "consult",
-        component: () => import("@/views/consult/index.vue"),
-      },
-      {
-        path: "/doctor",
-        name: "doctor",
-        component: () => import("@/views/consult/doctor.vue"),
-      },
-
-    
-
-
-
-]
-
-
-
-const router = createRouter(
-    {
-        history: createWebHistory(),
-        routes: routes
-    }
-)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
 
 router.beforeEach((to, from) => {
-    // 而不是去检查每条路由记录
-   
-    // to.matched.some(record => record.meta.requiresAuth)
-    // 这里需要空的
-    const Test=useTestStore()
-    const {role} =Test
-    // console.log('name is')
-    // console.log(name)
-    if (to.meta.requiresAuth&&!role) {
-        // console.log('to.meta.requiresAuth')
-        // console.log(useTestStore.name+'name!!!!!!11')
-      // 此路由需要授权，请检查是否已登录
-      // 如果没有，则重定向到登录页面
-      return {
-        path: '/login',
-        // 保存我们所在的位置，以便以后再来
-    
-        query: { redirect: to.fullPath },
-      }
+  // 而不是去检查每条路由记录
 
-     
-    }
-  })
-export default router
+  // to.matched.some(record => record.meta.requiresAuth)
+  // 这里需要空的
+  const Test = useTestStore();
+  const { role } = Test;
+  // console.log('name is')
+  // console.log(name)
+  if (to.meta.requiresAuth && !role) {
+    // console.log('to.meta.requiresAuth')
+    // console.log(useTestStore.name+'name!!!!!!11')
+    // 此路由需要授权，请检查是否已登录
+    // 如果没有，则重定向到登录页面
+    return {
+      path: "/login",
+      // 保存我们所在的位置，以便以后再来
+
+      query: { redirect: to.fullPath },
+    };
+  }
+});
+export default router;
---
+++ vite-project/src/views/search/search.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="display-flex">
+    <el-table
+      :data="tableData"
+      border
+      style="width: 35%; margin-top: 50px; margin-left: 100px"
+    >
+      <el-table-column prop="name" label="姓名" width="180" />
+      <el-table-column prop="email" label="联系方式" width="180" />
+      <el-table-column label=" " width="120">
+        <template #default="{ row }">
+          <el-button>{{ row.tag }}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-input
+      v-model="textarea"
+      :rows="15"
+      type="textarea"
+      placeholder="您好，想咨询什么信息？"
+      style="width: 30%; margin-left: 120px; margin-top: 40px"
+    />
+    <el-button
+      type="primary"
+      style="margin-top: 327px; margin-left: 30px"
+      plain
+      @click="goToConsult"
+      >发送</el-button
+    >
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import router from "../../router";
+import { defineComponent } from "vue";
+import { RouteLocationRaw, useRouter } from "vue-router";
+
+const textarea = ref("");
+
+const tableData = [
+  {
+    email: "QWE@QQ.COM",
+    name: "QWE",
+    tag: "咨询",
+  },
+  {
+    email: "ASD@QQ.COM",
+    name: "ASD",
+    tag: "咨询",
+  },
+  {
+    email: "AXC@QQ.COM",
+    name: "AXC",
+    tag: "咨询",
+  },
+  {
+    email: "ZZZ@QQ.COM",
+    name: "ZZZ",
+    tag: "咨询",
+  },
+];
+
+const goToConsult = () => {
+  const location: RouteLocationRaw = {
+    path: "../consult",
+  };
+  router.push(location);
+};
+</script>
+<style lang="scss" scoped>
+.display-flex {
+  display: flex;
+}
+</style>
